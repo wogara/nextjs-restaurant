d7393e75813ab6d5186a3d1346f3cb4f1a228a1b
wogara <weogara@gmail.com>
2024-05-27 15:04:35 -0400

    first react regular component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
+
+import Header from '@/components/header';
 
 export default function Home() {
-  console.log('Executing');
-return (
+  console.log("Executing");
+  return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p> Let&apos;s get started! </p>
-      <p><Link href='/about'>about</Link></p>
+      <p>
+        <Link href="/about">about</Link>
+      </p>
     </main>
   );
 }
